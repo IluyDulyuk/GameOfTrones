2bc68ac14e8b8d400c4a508f0cf47a31f519beae
IluyDulyuk <65189857+IluyDulyuk@users.noreply.github.com>
2021-09-22 22:16:35 +0700

    charRandom loading and add errorMassage(dev)

---
+++ src/components/errorMassage/errorMassage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './errorMassage.css';
+
+const ErrorMassage = () => {
+    
+    
+    return (
+        <>
+            <div className="error">
+                <div className="error__text">Произошла ошибка</div>
+            </div>
+        </>
+    )
+}
+
+export default ErrorMassage;
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+
+
+    return (
+        <div class="loadingio-spinner-pulse-mzgwn166a2">
+            <div class="ldio-17ulmi9zpd4h">
+                <div></div><div></div><div></div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Spinner;
